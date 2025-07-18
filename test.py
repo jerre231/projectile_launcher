a4a3f5ed05830aa24201e2ffa0e73055bfced2e8
MaquinaDeCoxinha <jvpvizeu@gmail.com>
2025-07-18 13:41:15 -0300

    cleaned code for submission

--- test.py
+++ test.py
@@ -2,27 +2,14 @@ import lib
 import numpy as np
 import gui
 
-"""
-# Definir parâmetros físicos
-params = {
-    'massa': 1,             # kg (ex: bola de baseball)           # m² (ex: área frontal de baseball)
-    'coeficiente_atrito': lib.calcularCoeficienteAtrito(0.5),         # Coeficiente de arrasto (esfera de 1 m de raio)
-    'gravidade': 9.80665,    # m/s²
-    'rotacao': np.pi/43200,      # kg/m³ (ao nível do mar)
-    'tempo_de_forca': 1.5*(10**-3) # 1.5 ms já convertido em segundos
-}
-print(params['coeficiente_atrito'])
-"""
 def run_simulation(params):
     # Instanciando a classe
     sim = lib.SimuladorProjetil(**params)
-    #sim.plotar_trajetoria()
+
     #sim.plotar_trajetoria_animada_comparada()
     sim.comparar_metodos()
 
-    # TODO: quebra entre 340k e 350k Newtons de forca, porque?
-
-def sim_comparison(params):   # TODO: Definir esta função
+def sim_comparison(params):
     sim = lib.SimuladorProjetil(**params)
 
     # Testando configuracoes
